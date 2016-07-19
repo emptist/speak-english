455aaec8f840539f8be8c5d5203d281652f6dca5
Yuki <uniquey_1987@163.com>
2016-07-19 16:19:10 +0800

    0.0.1 sound not success

--- renderer.js
+++ renderer.js
@@ -2,9 +2,11 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 var text;
+var say = require('say');
 $('button').click(function(event){
   text = $('textarea').val();
   $('#content').html(text)
+  say.speak('Hello!')
 })
 
 $('textarea').keypress(function(event){
